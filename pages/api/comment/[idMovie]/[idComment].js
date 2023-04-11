56eec8c4a28db002e2991fa4a73a339fa34705cc
Johan Hagnere <johan.hagnere@gmail.com>
2023-04-11 13:51:09 +0200

    get movieid commentid

--- pages/api/comment/[idMovie]/[idComment].js
+++ pages/api/comment/[idMovie]/[idComment].js
@@ -1,47 +1,49 @@
-import clientPromise from "../../../lib/mongodb";
+import clientPromise from "../../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
-// PAGE A MODIFIER JAI JUSTE COPIER COLLER IL FAUT FAIRE GET POST PUT DELETE DES SINGLE COMMENT ICI
-
 export default async function handler(req, res) {
-    const client = await clientPromise;
-    const db = client.db("sample_mflix");
-    const dbComment = db.collection("comments");
+  const client = await clientPromise;
+  const db = client.db("sample_mflix");
+  const dbComment = db.collection("comments");
 
-    const fakeComment = {
-        "name": "Jean Dupont",
-        "email": "jean.dupont@fakegmail.com",
-        "movie_id": "573a1390f29313caabcd4323",
-        "text": "Eiu quaerat fuga temporibus. Praesentium expedita sequi repellat id. Corporis minima enim ex. Provident fugit nisi dignissimos nulla nam ipsum aliquam.",
-        "date": "2002-08-18T04:56:07.000Z"
-    }
+  const fakeComment = {
+    name: "Jean Dupont",
+    email: "jean.dupont@fakegmail.com",
+    movie_id: "573a1390f29313caabcd4323",
+    text: "Eiu quaerat fuga temporibus. Praesentium expedita sequi repellat id. Corporis minima enim ex. Provident fugit nisi dignissimos nulla nam ipsum aliquam.",
+    date: "2002-08-18T04:56:07.000Z",
+  };
 
-    switch (req.method) {
-        case "GET":
-            const comments = await db.collection('comments').find({
-                movie_id: new ObjectId(req.query.idMovie.trim())
-            }).limit(10).toArray();
-            res.json({ status: 200, data: comments });
-            break;
-        case "POST":
-            const comment = await dbComment.insertOne({ ...req.body, movie_id: new ObjectId(req.body.movie_id) });
-            res.json({ status: 200, data: { comment: comment } })
-            //J'ai corrigé la requete pour qu'elle fonctionne, il fallait parser le movie_id en Object_id car il est passé en string dans le req.body
-            break;
-        case "PUT":
-            const { ...fieldsToUpdate } = req.body;
-            const updated = dbComment.findOneAndUpdate(
-                { _id: new ObjectId(req.query.id) },
-                { $set: fieldsToUpdate },
-                { new: true }
-                // NE RETOURNE RIEN ET DOIT RETOURNER LE DOC MODIFIE MAIS FONCTIONNE
-            );
-            res.json({ status: 200, data: updated })
-            // https://www.mongodb.com/docs/v6.0/reference/method/db.collection.findOneAndUpdate/#db.collection.findoneandupdate--
-            break;
-        case "DELETE":
-            await dbComment.deleteOne({ _id: new ObjectId(req.query.id) });
-            res.json({ status: 200, message: "comment successfully deleted" });
-            break;
-    }
+  switch (req.method) {
+    case "GET":
+      const comments = await dbComment.findOne({
+        _id: new ObjectId(req.query.idComment),
+        movie_id: new ObjectId(req.query.idMovie),
+      });
+      res.json({ status: 200, data: comments });
+      break;
+
+    case "POST":
+      const comment = await dbComment.insertOne({
+        ...req.body,
+        movie_id: new ObjectId(req.body.movie_id),
+      });
+      res.json({ status: 200, data: { comment: comment } });
+      break;
+    case "PUT":
+      const { ...fieldsToUpdate } = req.body;
+      const updated = dbComment.findOneAndUpdate(
+        { _id: new ObjectId(req.query.id) },
+        { $set: fieldsToUpdate },
+        { new: true }
+        // NE RETOURNE RIEN ET DOIT RETOURNER LE DOC MODIFIE MAIS FONCTIONNE
+      );
+      res.json({ status: 200, data: updated });
+      // https://www.mongodb.com/docs/v6.0/reference/method/db.collection.findOneAndUpdate/#db.collection.findoneandupdate--
+      break;
+    case "DELETE":
+      await dbComment.deleteOne({ _id: new ObjectId(req.query.id) });
+      res.json({ status: 200, message: "comment successfully deleted" });
+      break;
+  }
 }
--- pages/api/comments/[idMovie].js
+++ pages/api/comments/[idMovie].js
@@ -2,16 +2,18 @@ import clientPromise from "../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
-    const client = await clientPromise;
-    const db = client.db("sample_mflix");
-    const dbComment = db.collection("comments");
+  const client = await clientPromise;
+  const db = client.db("sample_mflix");
+  const dbComment = db.collection("comments");
 
-    switch (req.method) {
-        case "GET":
-            const comments = await dbComment.find({
-                movie_id: new ObjectId(req.query.idMovie.trim())
-            }).limit(10).toArray();
-            res.json({ status: 200, data: comments });
-            break;
-    }
+  switch (req.method) {
+    case "GET":
+      const comments = await dbComment
+        .find({
+          movie_id: new ObjectId(req.query.idMovie.trim()),
+        })
+        .toArray();
+      res.json({ status: 200, data: comments });
+      break;
+  }
 }
