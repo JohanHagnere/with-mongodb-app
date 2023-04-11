d9fe164e580149a32b2f60c1e4a960508c95ada2
Johan Hagnere <johan.hagnere@gmail.com>
2023-04-11 16:54:12 +0200

    wip swagger

--- pages/api/movie/[id].js
+++ pages/api/movie/[id].js
@@ -1,6 +1,178 @@
 import clientPromise from "../../../lib/mongodb";
 import { ObjectId } from "mongodb";
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Movie:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *        title:
+ *          type: string
+ *      required:
+ *          - _id
+ *          - title
+ *
+ *
+ *
+ *A FAIRE CONTINUER INDENTATION EN FONCTION DES VALEURS DESSOUS PUIS LES EFFACER
+ *
+ *
+ * Movie:
+ *      type: object
+ *    properties:
+ *        _id:
+ *           type: string
+ *         title:
+ *           type: string
+ *        year:
+ *           type: integer
+ *        genre:
+ *           type: array
+ *          items:
+ *             type: string
+ *        rated:
+ *           type: string
+ *         runtime:
+ *           type: integer
+ *        cast:
+ *           type: array
+ *          items:
+ *             type: string
+ *       required:
+ *         - _id
+ *        - title
+ *        - year
+ *        - genre
+ *        - rated
+ *        - runtime
+ *        - cast
+ *    NotFound:
+ *       type: object
+ *      properties:
+ *        message:
+ *          type: string
+ *      required:
+ *        - message
+ *    MovieInput:
+ *      type: object
+ *      properties:
+ *        title:
+ *          type: string
+ *        year:
+ *          type: integer
+ *        genre:
+ *          type: array
+ *          items:
+ *            type: string
+ *        rated:
+ *          type: string
+ *        runtime:
+ *          type: integer
+ *        cast:
+ *          type: array
+ *          items:
+ *            type: string
+ *      required:
+ *        - title
+ *        - year
+ *        - genre
+ *        - rated
+ *        - runtime
+ *        - cast
+ */
+
+/**
+ * @swagger
+ * /api/movie/{id}:
+ *   get:
+ *     summary: Get a movie by ID
+ *     description: Retrieve a movie from the database by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the movie to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
+ *       404:
+ *         description: Movie not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/NotFound'
+ *   put:
+ *     summary: Update a movie by ID
+ *     description: Update a movie in the database by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the movie to update.
+ *         schema:
+ *           type: string
+ *       - in: body
+ *         name: movie
+ *         description: The movie to update.
+ *         schema:
+ *           $ref: '#/components/schemas/MovieInput'
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Movie'
+ *       404:
+ *         description: Movie not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/NotFound'
+ *   delete:
+ *     summary: Delete a movie by ID
+ *     description: Delete a movie from the database by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the movie to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: number
+ *                   description: HTTP status code for the response
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   description: Success message
+ *                   example: "Movie successfully deleted"
+ *       404:
+ *         description: Movie not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/NotFound'
+ */
+
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db("sample_mflix");
@@ -12,7 +184,6 @@ export default async function handler(req, res) {
       res.json({ status: 200, data: { movie: data } });
       break;
     case "PUT":
-      // const idMovie = req.query.id
       const { ...fieldsToUpdate } = req.body;
       const updated = dbMovie.findOneAndUpdate(
         { _id: new ObjectId(req.query.id) },
