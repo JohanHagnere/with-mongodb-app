dab6f5fc98a3bc8862022eeefa40c863c16d9aec
Johan Hagnere <johan.hagnere@gmail.com>
2023-04-25 09:28:00 +0200

    movies

--- app/components/Movies.jsx
+++ app/components/Movies.jsx
@@ -12,14 +12,29 @@ function Movies() {
   }, []);
 
   return (
-    <div>
+    <>
       <h2>Movies</h2>
-      <ul>
+      <div
+        className="flex__cards"
+        style={{ display: "flex", flexWrap: "wrap", gap: "2rem" }}
+      >
         {movies.map((movie) => (
-          <li key={movie.title}>{movie.title}</li>
+          <div
+            className="card"
+            key={movie.title}
+            style={{ display: "inlineBlock", width: 200 }}
+          >
+            <h3 className="card__title">{movie.title}</h3>
+            <p className="card__plot">{movie.plot}</p>
+            <img
+              src={movie.poster}
+              alt={movie.title}
+              style={{ width: "100%" }}
+            />
+          </div>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
 
